9361d41ffe7bed5a218cf132796f09be099eeb39
Victor D'Avila <vavila@uolinc.com>
2022-05-25 17:29:40 -0300

    add menu animate

--- app/src/main/java/com/example/homeanimationpoc/MainActivity.kt
+++ app/src/main/java/com/example/homeanimationpoc/MainActivity.kt
@@ -28,6 +28,8 @@ class MainActivity : AppCompatActivity() {
     private val transformAlphaAnimation by lazy {
         TransformAlphaAnimation.TransformAlphaBuilder()
             .setFillAfter(true)
+            .setTransformDownStartListener { fab.isClickable = false }
+            .setTransformUpStartListener { fab.isClickable = true }
             .build()
     }
 
@@ -36,79 +38,40 @@ class MainActivity : AppCompatActivity() {
             .build()
     }
 
-    private val transformTranslateRightDownAnimation by lazy {
-        TransformTranslateAnimation.TransformTranslateBuilder()
-            .setStartTranslateXValue(fab.translationX)
-            .setEndTranslateXValue(-(card1.width / 2f))
-            .setStartTranslateYValue(fab.translationY)
-            .setEndTranslateYValue(-(card1.height / 2f))
-            //.setTransformUpStartListener { card4.isVisible = true }
-            .build()
+    private val animationSet1 by lazy {
+        createAnimation(
+            card1.translationX,
+            -(screenWidth - card1.measuredWidth + 30).toFloat(),
+            card1.translationY,
+            -(card1.measuredHeight + 50 + (card1.measuredHeight / 3f))//((card1.height / 2f) + card1.height + 16)
+        ) { card1.isVisible = true }
     }
 
-    private val transformTranslateRightUpAnimation by lazy {
-        TransformTranslateAnimation.TransformTranslateBuilder()
-            .setStartTranslateXValue(fab.translationX)
-            .setEndTranslateXValue(-(card1.width / 2f))
-            .setStartTranslateYValue(fab.translationY)
-            .setEndTranslateYValue(-((card1.height / 2f) + card1.height + 16))
-            //.setTransformUpStartListener { card2.isVisible = true }
-            .build()
+    private val animationSet2 by lazy {
+        createAnimation(
+            card1.translationX,
+            -((screenWidth / 2) - card1.measuredWidth + 10).toFloat(),
+            card1.translationY,
+            -(card1.measuredHeight + 50 + (card1.measuredHeight / 3f))
+        ) { card2.isVisible = true }
     }
 
-    private val transformTranslateLeftDownAnimation by lazy {
-        TransformTranslateAnimation.TransformTranslateBuilder()
-            .setStartTranslateXValue(fab.translationX)
-            .setEndTranslateXValue(-(screenWidth - card1.width) + 16f)
-            .setStartTranslateYValue(fab.translationY)
-            .setEndTranslateYValue(-(card1.height / 2f))
-            //.setTransformUpStartListener { card3.isVisible = true }
-            .build()
+    private val animationSet3 by lazy {
+        createAnimation(
+            card1.translationX,
+            -(screenWidth - card1.measuredWidth + 30).toFloat(),
+            card1.translationY,
+            -(card1.measuredHeight / 3f)
+        ) { card3.isVisible = true }
     }
 
-    private val transformTranslateLeftUpAnimation by lazy {
-        TransformTranslateAnimation.TransformTranslateBuilder()
-            .setStartTranslateXValue(fab.translationX)
-            .setEndTranslateXValue(-(screenWidth - card1.width) + 16f)
-            .setStartTranslateYValue(fab.translationY)
-            .setEndTranslateYValue(-((card1.height / 2f) + card1.height + 16))
-            //.setTransformUpStartListener { card1.isVisible = true }
-            .build()
-    }
-
-    private val transformRightUpSet by lazy {
-        TransformAnimationsSet.TransformsSetBuilder()
-            .addUpAnimation(transformScaleAnimation.getUpAnimation())
-            .addUpAnimation(transformTranslateRightUpAnimation.getUpAnimation())
-            .addDownAnimation(transformScaleAnimation.getDownAnimation())
-            .addDownAnimation(transformTranslateRightUpAnimation.getDownAnimation())
-            .buildSetTransform()
-    }
-    private val transformRightDownSet by lazy {
-        TransformAnimationsSet.TransformsSetBuilder()
-            .addUpAnimation(transformScaleAnimation.getUpAnimation())
-            .addUpAnimation(transformTranslateRightDownAnimation.getUpAnimation())
-            .addDownAnimation(transformScaleAnimation.getDownAnimation())
-            .addDownAnimation(transformTranslateRightDownAnimation.getDownAnimation())
-            .buildSetTransform()
-    }
-
-    private val transformLeftUpSet by lazy {
-        TransformAnimationsSet.TransformsSetBuilder()
-            .addUpAnimation(transformTranslateLeftUpAnimation.getUpAnimation())
-            .addUpAnimation(transformScaleAnimation.getUpAnimation())
-            .addDownAnimation(transformTranslateLeftUpAnimation.getDownAnimation())
-            .addDownAnimation(transformScaleAnimation.getDownAnimation())
-            .buildSetTransform()
-    }
-
-    private val transformLeftDownSet by lazy {
-        TransformAnimationsSet.TransformsSetBuilder()
-            .addUpAnimation(transformTranslateLeftDownAnimation.getUpAnimation())
-            .addUpAnimation(transformScaleAnimation.getUpAnimation())
-            .addDownAnimation(transformTranslateLeftDownAnimation.getDownAnimation())
-            .addDownAnimation(transformScaleAnimation.getDownAnimation())
-            .buildSetTransform()
+    private val animationSet4 by lazy {
+        createAnimation(
+            card1.translationX,
+            -((screenWidth / 2) - card1.measuredWidth + 10).toFloat(),
+            card1.translationY,
+            -(card1.measuredHeight / 3f)
+        ) { card4.isVisible = true }
     }
 
     private val screenWidth by lazy {
@@ -121,115 +84,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val configuration = resources.configuration
-        val screenWidthDp = configuration.screenWidthDp
-        val screenHeightDp = configuration.screenHeightDp
-
-        card1.measuredWidth
-
-        val animate = TransformTranslateAnimation.TransformTranslateBuilder()
-            .setStartTranslateXValue(card1.translationX)
-            .setEndTranslateXValue(-(screenWidth - card1.measuredWidth).toFloat())
-            .setStartTranslateYValue(card1.translationY)
-            .setEndTranslateYValue(-((card1.height / 2f) + card1.height + 16))
-            .setTransformUpStartListener {
-                card1.isVisible = true
-            }
-            .build()
-
-        val scale = TransformScaleAnimation.TransformScaleBuilder()
-            //.setTransformUpStartListener { card1.isVisible = true }
-            .build()
-
         card1.setOnClickListener {
-            transformAlphaAnimation.transformDown(fab)
-        }
-
-        card2.setOnClickListener {
             transformAlphaAnimation.transformUp(fab)
-        }
-
-        fab.setOnClickListener {
-            val animateTranslate1 = TransformTranslateAnimation.TransformTranslateBuilder()
-                .setStartTranslateXValue(card1.translationX)
-                .setEndTranslateXValue(-(screenWidth - card1.measuredWidth + 30).toFloat())
-                .setStartTranslateYValue(card1.translationY)
-                .setEndTranslateYValue(-(card1.measuredHeight + 50 + (card1.measuredHeight / 3f)))
-                .setTransformUpStartListener {
-                    card1.isVisible = true
-                }
-                .build()
-
-            val animateTranslate2 = TransformTranslateAnimation.TransformTranslateBuilder()
-                .setStartTranslateXValue(card1.translationX)
-                .setEndTranslateXValue(-((screenWidth / 2) - card1.measuredWidth + 10).toFloat())
-                .setStartTranslateYValue(card1.translationY)
-                .setEndTranslateYValue(-(card1.measuredHeight + 50 + (card1.measuredHeight / 3f)))
-                .setTransformUpStartListener {
-                    card2.isVisible = true
-                }
-                .build()
-
-            val animateTranslate3 = TransformTranslateAnimation.TransformTranslateBuilder()
-                .setStartTranslateXValue(card1.translationX)
-                .setEndTranslateXValue(-(screenWidth - card1.measuredWidth + 30).toFloat())
-                .setStartTranslateYValue(card1.translationY)
-                .setEndTranslateYValue(-(card1.measuredHeight / 3f))
-                .setTransformUpStartListener {
-                    card3.isVisible = true
-                }
-                .build()
-
-            val animateTranslate4 = TransformTranslateAnimation.TransformTranslateBuilder()
-                .setStartTranslateXValue(card1.translationX)
-                .setEndTranslateXValue(-((screenWidth / 2) - card1.measuredWidth + 10).toFloat())
-                .setStartTranslateYValue(card1.translationY)
-                .setEndTranslateYValue(-(card1.measuredHeight / 3f))
-                .setTransformUpStartListener {
-                    card4.isVisible = true
-                }
-                .build()
-
-            val animate1 = TransformAnimationsSet.TransformsSetBuilder()
-                .addUpAnimation(animateTranslate1.getUpAnimation())
-                .addUpAnimation(scale.getUpAnimation())
-                .addDownAnimation(animateTranslate1.getDownAnimation())
-                .addDownAnimation(scale.getDownAnimation())
-                .buildSetTransform()
-
-            val animate2 = TransformAnimationsSet.TransformsSetBuilder()
-                .addUpAnimation(animateTranslate2.getUpAnimation())
-                .addUpAnimation(scale.getUpAnimation())
-                .addDownAnimation(animateTranslate2.getDownAnimation())
-                .addDownAnimation(scale.getDownAnimation())
-                .buildSetTransform()
-
-            val animate3 = TransformAnimationsSet.TransformsSetBuilder()
-                .addUpAnimation(animateTranslate3.getUpAnimation())
-                .addUpAnimation(scale.getUpAnimation())
-                .addDownAnimation(animateTranslate3.getDownAnimation())
-                .addDownAnimation(scale.getDownAnimation())
-                .buildSetTransform()
-
-            val animate4 = TransformAnimationsSet.TransformsSetBuilder()
-                .addUpAnimation(animateTranslate4.getUpAnimation())
-                .addUpAnimation(scale.getUpAnimation())
-                .addDownAnimation(animateTranslate4.getDownAnimation())
-                .addDownAnimation(scale.getDownAnimation())
-                .buildSetTransform()
 
             isUpTransform = if (isUpTransform) {
-                animate1.transformUp(card1)
-                animate2.transformUp(card2)
-                animate3.transformUp(card3)
-                animate4.transformUp(card4)
+                animationSet1.transformUp(card1)
+                animationSet2.transformUp(card2)
+                animationSet3.transformUp(card3)
+                animationSet4.transformUp(card4)
+
+                transformAlphaAnimation.transformDown(fab)
 
                 false
             } else {
-                animate1.transformDown(card1)
-                animate2.transformDown(card2)
-                animate3.transformDown(card3)
-                animate4.transformDown(card4)
+                animationSet1.transformDown(card1)
+                animationSet2.transformDown(card2)
+                animationSet3.transformDown(card3)
+                animationSet4.transformDown(card4)
+
+                transformAlphaAnimation.transformUp(fab)
+
+                true
+            }
+        }
+
+        fab.setOnClickListener {
+            isUpTransform = if (isUpTransform) {
+                animationSet1.transformUp(card1)
+                animationSet2.transformUp(card2)
+                animationSet3.transformUp(card3)
+                animationSet4.transformUp(card4)
+
+                transformAlphaAnimation.transformDown(fab)
+
+                false
+            } else {
+                animationSet1.transformDown(card1)
+                animationSet2.transformDown(card2)
+                animationSet3.transformDown(card3)
+                animationSet4.transformDown(card4)
+
+                transformAlphaAnimation.transformUp(fab)
 
                 true
             }
@@ -255,4 +150,26 @@ class MainActivity : AppCompatActivity() {
         itemDecor.attachToRecyclerView(list)
         list.adapter = adapter
     }
+
+    private fun createAnimation(startXValue: Float, endXValue: Float, startYValue: Float, endYValue: Float, startListener: (() -> Unit)? = null): TransformAnimationsSet {
+        val translateAnimation = createTranslateAnimation(startXValue, endXValue, startYValue, endYValue, startListener)
+        return createAnimationSet(translateAnimation)
+    }
+
+    private fun createTranslateAnimation(startXValue: Float, endXValue: Float, startYValue: Float, endYValue: Float, startListener: (() -> Unit)?) =
+        TransformTranslateAnimation.TransformTranslateBuilder()
+            .setStartTranslateXValue(startXValue)
+            .setEndTranslateXValue(endXValue)
+            .setStartTranslateYValue(startYValue)
+            .setEndTranslateYValue(endYValue)
+            .setTransformUpStartListener(startListener)
+            .build()
+
+    private fun createAnimationSet(transformAnimation: TransformAnimation) =
+        TransformAnimationsSet.TransformsSetBuilder()
+            .addUpAnimation(transformAnimation.getUpAnimation())
+            .addUpAnimation(transformScaleAnimation.getUpAnimation())
+            .addDownAnimation(transformAnimation.getDownAnimation())
+            .addDownAnimation(transformScaleAnimation.getDownAnimation())
+            .buildSetTransform()
 }
